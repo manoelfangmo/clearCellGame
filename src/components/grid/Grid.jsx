4ab76bbe90e0a391ae20c82e4914f2b01cfc4441
Manoel <manoelfangmo@gmail.com>
2021-08-08 21:37:16 -0400

    Problem with copyColors

--- src/components/grid/Grid.jsx
+++ src/components/grid/Grid.jsx
@@ -6,7 +6,7 @@ import Modal from "../modal/Modal"
 export default function Grid() {
     const [isOpen, setIsOpen] = useState(true)
     const [length, setLength] = useState(10);
-    const [width, setWidth] = useState(5);
+    const [width, setWidth] = useState(3);
     const numOfCells = length * width;
     const cells = [];
     for (let i = 0; i < numOfCells; i++){
@@ -29,10 +29,39 @@ export default function Grid() {
     const [start, setStart] = useState(false)
     useEffect(() => {
         if(start){
-            document.getElementById("cell0").style.backgroundColor = colors[0]; 
+            console.log(length)
+            console.log(width)
+            colorRow(0)
+            copyColors(5)
         }
         
-    })
+    },)
+
+    const colorRow = (startIndex) => {
+        for(let i = startIndex; i<width; i++){
+            const colorIndex = Math.floor((Math.random() * colors.length));
+            document.getElementById("cell"+i).style.backgroundColor = colors[colorIndex]; 
+        }
+    }
+    const copyColors = (startIndex) => {
+        for(let i = startIndex; i<(startIndex + width); i++){
+            console.log(width)
+            document.getElementById("cell"+i).style.backgroundColor = 
+            document.getElementById("cell"+(i-width)).style.backgroundColor;
+
+        }
+    }
+
+    // const bottomIsEmpty = () => {
+    //     let isEmpty = false;
+    //     for(let i = numOfCells-width; i< numOfCells; i++){
+    //         if(document.getElementById("cell"+i).style.backgroundColor === "white"){
+    //             isEmpty = true;
+    //             console.log(document.getElementById("cell"+i).style.backgroundColor)
+    //         }
+    //     }
+    //     return isEmpty;
+    // }
 
     return (
         <>
