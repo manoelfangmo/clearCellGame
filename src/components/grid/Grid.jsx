924e92467f2de0b8e1c049d2d79649e4781cf97a
Manoel <manoelfangmo@gmail.com>
2021-08-21 14:44:25 -0400

    Lag fixed

--- src/components/grid/Grid.jsx
+++ src/components/grid/Grid.jsx
@@ -1,285 +1,262 @@
-import React from 'react'
-import "./grid.scss"
-import {useState, useEffect} from "react"
-import Modal from "../modal/Modal"
-import ScoreBoard from '../scoreBoard/ScoreBoard'
-import GameOver from '../gameOver/GameOver'
-
-
-
+import React from "react";
+import "./grid.scss";
+import { useState, useEffect, useRef } from "react";
+import Modal from "../modal/Modal";
+import ScoreBoard from "../scoreBoard/ScoreBoard";
+import GameOver from "../gameOver/GameOver";
 
 export default function Grid() {
+  const [isOpen, setIsOpen] = useState(true);
+  const [startGame, setStartGame] = useState(false);
+  const [length, setLength] = useState(10);
+  const [width, setWidth] = useState(5);
+  const [score, setScore] = useState(0);
+  const [coordinates, setCoordinates] = useState({
+    row: 0,
+    col: 0,
+    clicked: false,
+  });
+  const [gameOver, setGameOver] = useState(false);
+  const [speed, setSpeed] = useState(1500);
 
-    
-    const [isOpen, setIsOpen] = useState(true);
-    const [startGame, setStartGame] = useState(false);
-    const [length, setLength] = useState(10);
-    const [width, setWidth] = useState(5);
-    const [score, setScore] = useState(0);
-    const [coordinates, setCoordinates] = useState({row:0,col:0,clicked:false})
-    const [gameOver, setGameOver] = useState(false)
-    const [speed, setSpeed] = useState(1000)
+  const configGrid = (newLength, newWidth) => {
+    setLength(Number(newLength));
+    setWidth(Number(newWidth));
+  };
+  const configSpeed = (difficulty) => {
+    if (difficulty === "Hard") {
+      setSpeed(500);
+    } else if (difficulty === "Medium") {
+      setSpeed(1000);
+    } else {
+      setSpeed(1500);
+    }
+  };
 
-    const configGrid = (newLength, newWidth) => {
-        setLength(Number(newLength));
-        setWidth(Number(newWidth));
-        
+  function createMap(columnCount, rowCount) {
+    const map = [];
+    for (let x = 0; x < columnCount; x++) {
+      map[x] = []; // set up inner array
+      for (let y = 0; y < rowCount; y++) {
+        addCell(map, x, y);
+      }
     }
-    const configSpeed = (newSpeed) => {
-        setSpeed(Number(newSpeed));
+    return map;
+  }
+
+  function addCell(map, x, y) {
+    const cellProperties = ["cell", "cell" + x + y, "white", x, y];
+
+    map[x][y] = cellProperties; // create a new object on x and y
+  }
+  // {className: "cell", id: "cell"+x + y, style:{backgroundColor: "white"}}
+
+  const gridStyle = {
+    gridTemplateColumns: " ",
+    gridTemplateRows: " ",
+  };
+  for (let i = 0; i < length; i++) {
+    gridStyle.gridTemplateRows += "7% ";
+  }
+  for (let i = 0; i < width; i++) {
+    gridStyle.gridTemplateColumns += "7% ";
+  }
+  const gridRef = useRef();
+
+  gridRef.current = createMap(length, width);
+
+  const colors = ["blue", "red", "yellow", "green"];
+  const randomColor = () => {
+    const colorIndex = Math.floor(Math.random() * colors.length);
+    const color = colors[colorIndex];
+    return color;
+  };
+
+  const isGameOver = () => {
+    let gameOver = false;
+    const lastRow = length - 1;
+    for (let i = 0; i < width; i++) {
+      if (
+        document.getElementById("cell" + lastRow + i).style.backgroundColor !==
+        "white"
+      ) {
+        setGameOver(true);
+        gameOver = true;
+        break;
+      }
     }
-    
-    function createMap(columnCount, rowCount) {
-        const map = [];
-        for (let x = 0; x < columnCount; x++) {
-          map[x] = []; // set up inner array
-          for (let y = 0; y < rowCount; y++) {
-             addCell(map, x, y);
+    return gameOver;
+  };
+
+  useEffect(() => {
+    if (startGame && isGameOver() === false) {
+      const intervalId = setInterval(() => {
+        nextAnimationStep();
+      }, speed);
+      return () => {
+        clearInterval(intervalId);
+      };
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [startGame]);
+
+  const nextAnimationStep = () => {
+    if (isGameOver() === false) {
+      //Copy domStyles to gridRef
+      const backgroundColor = 2;
+      for (let i = 0; i < length; i++) {
+        for (let j = 0; j < width; j++) {
+          gridRef.current[i][j][backgroundColor] = document.getElementById(
+            "cell" + i + j
+          ).style.backgroundColor;
+        }
+      }
+
+      for (let i = 0; i < length - 1; i++) {
+        let h = i + 1;
+        for (let j = 0; j < width; j++) {
+          document.getElementById("cell" + h + j).style.backgroundColor =
+            gridRef.current[i][j][backgroundColor];
+        }
+      }
+
+      for (let i = 0; i < width; i++) {
+        document.getElementById("cell" + 0 + i).style.backgroundColor =
+          randomColor();
+      }
+
+      // setGrid(newGrid)
+    }
+  };
+
+  const processCellHelper = (surrRow, surrCol, row, col) => {
+    if (
+      !isOutOfBounds(surrRow, surrCol) &&
+      document.getElementById("cell" + surrRow + surrCol).style
+        .backgroundColor ===
+        document.getElementById("cell" + row + col).style.backgroundColor
+    ) {
+      document.getElementById(
+        "cell" + surrRow + surrCol
+      ).style.backgroundColor = "white";
+      setScore((prevScore) => prevScore + 1);
+    }
+  };
+  const processCell = (row, col) => {
+    if (
+      document.getElementById("cell" + row + col).style.backgroundColor !==
+      "white"
+    ) {
+      processCellHelper(row - 1, col, row, col);
+      processCellHelper(row + 1, col, row, col);
+      processCellHelper(row - 1, col - 1, row, col);
+      processCellHelper(row + 1, col + 1, row, col);
+      processCellHelper(row, col - 1, row, col);
+      processCellHelper(row, col + 1, row, col);
+      processCellHelper(row + 1, col - 1, row, col);
+      processCellHelper(row - 1, col + 1, row, col);
+      document.getElementById("cell" + row + col).style.backgroundColor =
+        "white";
+      setScore((prevScore) => prevScore + 1);
+    }
+    for (let i = length - 2; i > 0; i--) {
+      if (rowIsEmpty(i)) {
+        //Copy domStyles to gridRef
+        const backgroundColor = 2;
+        for (let i = 0; i < length; i++) {
+          for (let j = 0; j < width; j++) {
+            gridRef.current[i][j][backgroundColor] = document.getElementById(
+              "cell" + i + j
+            ).style.backgroundColor;
           }
         }
-        return map;
-    }
-     
-    
-    function addCell(map, x, y) {
-        const cellProperties = {className: "cell", id: "cell" + x +y, row: x, column: y, style:{backgroundColor:"white",set: (newValue) => {
-            this.backgroundColor = newValue;
-        } }};
-        
-         map[x][y] = cellProperties ; // create a new object on x and y
-         
-    }
-    // {className: "cell", id: "cell"+x + y, style:{backgroundColor: "white"}}
-    
-    
-    const [grid, setGrid] = useState([]);
-
-    const gridStyle = { 
-        gridTemplateColumns: " ",
-        gridTemplateRows: " ",
-    
-    }
-    for (let i=0; i< length; i++){
-        gridStyle.gridTemplateRows += "7% "
-    }
-    for (let i=0; i< width; i++){
-        gridStyle.gridTemplateColumns += "7% "
-    }
-
-    useEffect(() => {
-        setGrid(createMap(length,width))
-        
-    },[length,width])
-
-
-    
-
-    const colors = [ "blue", "red", "yellow", "green"];
-    const randomColor = () => { 
-        const colorIndex = Math.floor((Math.random() * colors.length));
-        const style = {backgroundColor: colors[colorIndex]};
-        return style
-           
-    
-    }
-
-
-    const  isGameOver = () => {
-        let gameOver = false;
-        const lastRow = length-1;
-        for(let i = 0; i< width; i++){      
-            if(grid[lastRow][i].style.backgroundColor !== "white"){
-                setGameOver(true)
-                gameOver = true;
-                break;
-                
-            }
+        for (let breakPoint = i; breakPoint < length - 1; breakPoint++) {
+          for (let j = 0; j < width; j++) {
+            document.getElementById(
+              "cell" + breakPoint + j
+            ).style.backgroundColor =
+              gridRef.current[breakPoint + 1][j][backgroundColor];
+            document.getElementById(
+              "cell" + (breakPoint + 1) + j
+            ).style.backgroundColor = "white";
+          }
         }
-        return gameOver;
+      }
     }
+  };
 
-
-    useEffect(() => {
-    
-        if(startGame && isGameOver() === false){
-            const intervalId = setInterval(() => {
-                nextAnimationStep();
-                
-        
-            }, speed);
-                return () => {clearInterval(intervalId); 
-           }
-        }
-    
-    }, [startGame, grid])
-  
-    const nextAnimationStep = () => {
-        if(isGameOver() === false){
-            const gridCopy = JSON.parse(JSON.stringify(grid))
-            const newGrid = JSON.parse(JSON.stringify(grid))
-        
-        
-
-            for(let i = 0; i< length-1; i++) {
-                 newGrid[i+1] = JSON.parse(JSON.stringify(gridCopy[i]))
-                for(let j= 0; j< width; j++) {
-                    newGrid[i+1][j].row = gridCopy[i+1][j].row;
-                }
-            }
-            
-            for(let i = 0; i< width; i++){
-                newGrid[0][i].style = randomColor();
-             } 
-
-             
-            setGrid(newGrid);
-                     
-            // setGrid(newGrid)
-            
-        }
+  const isOutOfBounds = (row, col) => {
+    let outOfBounds = false;
+    if (row < 0 || row >= length) {
+      outOfBounds = true;
+    } else if (col < 0 || col >= width) {
+      outOfBounds = true;
     }
+    return outOfBounds;
+  };
 
-    const processCell = (row, col) =>{
-        console.log("process", row, col)
-        const gridCopy = JSON.parse(JSON.stringify(grid))
-        let tempScore = 0;
-        if(gridCopy[row][col].style.backgroundColor !== "white") {
-			if(	!isOutOfBounds(row-1,col)&&
-            gridCopy[row-1][col].style.backgroundColor === gridCopy[row][col].style.backgroundColor) {
-				gridCopy[row-1][col].style = {backgroundColor: "white"};
-				tempScore++;
-			}
-
-			if(!isOutOfBounds(row+1,col) &&
-            gridCopy[row+1][col].style.backgroundColor ===  gridCopy[row][col].style.backgroundColor) {
-				gridCopy[row+1][col].style = {backgroundColor: "white"};
-				tempScore++;
-			}
-
-			if(!isOutOfBounds(row-1,col-1) &&
-            gridCopy[row-1][col-1].style.backgroundColor === gridCopy[row][col].style.backgroundColor) {
-				gridCopy[row-1][col-1].style = {backgroundColor: "white"};
-				tempScore++;
-			}
-
-			if(!isOutOfBounds(row+1,col+1) &&
-            gridCopy[row+1][col+1].style.backgroundColor === gridCopy[row][col].style.backgroundColor) {
-				gridCopy[row+1][col+1].style = {backgroundColor: "white"};
-				tempScore++;
-			}
-
-			if(!isOutOfBounds(row,col-1) &&
-             gridCopy[row][col-1].style.backgroundColor === gridCopy[row][col].style.backgroundColor) {
-				gridCopy[row][col-1].style = {backgroundColor: "white"};
-				tempScore++;
-
-			}
-
-			if(!isOutOfBounds(row,col+1) &&
-            gridCopy[row][col+1].style.backgroundColor === gridCopy[row][col].style.backgroundColor) {
-				gridCopy[row][col+1].style = {backgroundColor: "white"};
-				tempScore++;
-			}
-
-			if(!isOutOfBounds(row+1,col-1) &&
-            gridCopy[row+1][col-1].style.backgroundColor === gridCopy[row][col].style.backgroundColor) {
-				gridCopy[row+1][col-1].style = {backgroundColor: "white"};
-				tempScore++;
-			}
-
-			if(!isOutOfBounds(row-1,col+1) &&
-            gridCopy[row-1][col+1].style.backgroundColor === gridCopy[row][col].style.backgroundColor ) {
-				gridCopy[row-1][col+1].style = {backgroundColor: "white"};
-				tempScore++;
-			}
-			gridCopy[row][col].style = {backgroundColor: "white"};
-            tempScore++
-            
-    
-
-            setScore((prevScore)=> prevScore + tempScore)
-                
-
-		}
-        for(let i = length-2; i > 0; i--){
-            if(rowIsEmpty(i, gridCopy) === true){
-                console.log("row empty", i)
-                for(let breakPoint = i; breakPoint < length-1; breakPoint++){
-                    console.log("copying row", breakPoint+1,"to", breakPoint)
-                    console.log("setting row", breakPoint+1, "to white")
-                    for(let j = 0; j < width; j++){
-                        gridCopy[breakPoint][j] = JSON.parse(JSON.stringify(gridCopy[breakPoint+1][j]));
-                        gridCopy[breakPoint][j].row = breakPoint;
-                        gridCopy[breakPoint+1][j].style = {backgroundColor: "white"};
-                    }
-                }
-            } else {
-                console.log(" row not empty", i)
-            }
-        }
-        setGrid((prevGrid)=> gridCopy)
+  const rowIsEmpty = (rowNum) => {
+    for (let i = 0; i < width; i++) {
+      if (
+        document.getElementById("cell" + rowNum + i).style.backgroundColor !==
+        "white"
+      ) {
+        return false;
+      }
     }
-    const isOutOfBounds = (row, col) => {
-		let outOfBounds = false;
-		if(row < 0 || row >= length) {
-			outOfBounds = true;
-		}else if(col < 0 || col >= width) {
-			outOfBounds = true;
-		}
-		return outOfBounds;
+    return true;
+  };
 
-	}
+  useEffect(() => {
+    if (coordinates.clicked) {
+      processCell(coordinates.row, coordinates.col);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [coordinates]);
 
-    const rowIsEmpty = (rowNum, grid) => {
-		for (let i = 0; i< width; i++) {
-			if(grid[rowNum][i].style.backgroundColor !== "white") {
-				return false;
-			}
-		}
-		return true;
-	}
-
-    useEffect(() => {
-        if(coordinates.clicked){
-            processCell(coordinates.row,coordinates.col)
+  return (
+    <>
+      <div className="grid" style={gridStyle}>
+        {gridRef.current.map((cols) =>
+          cols.map((cells) => (
+            <span
+              className={cells[0]}
+              id={cells[1]}
+              style={{ backgroundColor: cells[2] }}
+              onClick={() =>
+                setCoordinates({
+                  row: cells[3],
+                  col: cells[4],
+                  clicked: true,
+                })
+              }
+            ></span>
+          ))
+        )}
+      </div>
+      <button onClick={() => setIsOpen(true)} className="settings">
+        Game Settings
+      </button>
+      <button
+        onClick={() =>
+          setStartGame(() => {
+            return true;
+          })
         }
-    
-    }, [coordinates])
-
-   
-
-    
-
-    
-
-
-
-
-    return (
-        <>
-            <div className="grid" style={gridStyle}>
-                 {grid.map((cols) => (
-                     cols.map((cells) => (
-                        <span className={cells.className} style={cells.style}
-                        onClick={() => setCoordinates({row: cells.row , col: cells.column, clicked: true})}
-                        // onClick={(a, b,c, d, e) => console.log("rowNUm", a, b, c, d,e)}
-                         id={cells.id}
-                        ></span>
-                     ))
-                     
-                 ))}
-
-            </div>
-            <button onClick={()=> setIsOpen(true)} className="settings">Game Settings</button>
-            <button onClick={()=> setStartGame(true)} className="start">Start Game</button>
-            <Modal open={isOpen} onClose={()=> setIsOpen(false)} length={length} 
-             width={width} configGrid={configGrid} configSpeed={configSpeed} />
-            <ScoreBoard score={score} />
-            <GameOver gameOver={gameOver} />
-        </>
-            
-
-
-      
-    )
+        className="start"
+      >
+        Start Game
+      </button>
+      <Modal
+        open={isOpen}
+        onClose={() => setIsOpen(false)}
+        length={length}
+        width={width}
+        configGrid={configGrid}
+        configSpeed={configSpeed}
+      />
+      <ScoreBoard score={score} />
+      <GameOver gameOver={gameOver} />
+    </>
+  );
 }
--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,86 +1,85 @@
-import React from 'react'
-import "./modal.scss"
-import ReactDom from 'react-dom'
-import {useState} from "react"
-export default function Modal({open, onClose, width, length, configGrid, configSpeed}) {
-    const maxLength = 10;
-    const maxWidth = 10;
-    
-    const [state, setState] = useState({width: width, length:length})
-    if(!open){
-        return null
-    }
-    const lengthList= [];
-    for(let i=1; i<= maxLength; i++){
-        lengthList.push(i);
-    }
-    const widthList= []
-    for(let i=1; i<= maxWidth; i++){
-        widthList.push(i);
-    }
+import React from "react";
+import "./modal.scss";
+import ReactDom from "react-dom";
+import { useState } from "react";
+export default function Modal({
+  open,
+  onClose,
+  width,
+  length,
+  configGrid,
+  configSpeed,
+}) {
+  const maxLength = 10;
+  const maxWidth = 10;
 
-    const handleChange = (e) => {
-        const name = e.target.name;
-        const value = e.target.value;
+  const [state, setState] = useState({ width: width, length: length });
+  const [difficulty, setDifficulty] = useState("Easy");
+  if (!open) {
+    return null;
+  }
+  const lengthList = [];
+  for (let i = 1; i <= maxLength; i++) {
+    lengthList.push(i);
+  }
+  const widthList = [];
+  for (let i = 1; i <= maxWidth; i++) {
+    widthList.push(i);
+  }
 
-       
-        state[name] = value;
-        setState({...state})
-        console.log("state", state)
-    }
-    
-    const handleSubmit = ()=> {
-        onClose();
-        configGrid(state.length, state.width);
-    }
-    const handleSpeedChange = (e) => {
-        const name = e.target.value;
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
 
-        if (name === "Hard"){
-            configSpeed(500)
-        }else if (name === "Medium"){
-            configSpeed(750)
-        }else{
-            configSpeed(1000)
-        }
+    state[name] = value;
+    setState({ ...state });
+  };
 
-    }
+  const handleSubmit = () => {
+    onClose();
+    configGrid(state.length, state.width);
+    configSpeed(difficulty);
+  };
+  const handleDifficulty = (e) => {
+    setDifficulty(e.target.value);
+  };
 
-    return ReactDom.createPortal(
-        <>
-                <div className="overlay"></div>
-                <div className="modal">
-                <form action="">
-                    <label htmlFor="length">Length:</label>
-                        <select name="length" value={state.length} onChange={handleChange}>
-                            {lengthList.map((d,index) => (
-                                <option value={d}>{d}</option>
-                            ))}
-                        </select> <br />
-
-                    <label htmlFor="width">Width:  </label>
-                        <select name="width" value={state.width} onChange={handleChange}>
-                            {widthList.map((d) => (
-                                <option value={d}>{d}</option>
-                            ))}
-
-                        </select> <br />
-
-                    <label htmlFor="speed">Speed:  </label>
-                        <select name="length" value="Easy" onChange={handleSpeedChange}>
-                                <option value="Easy">Easy</option>
-                                <option value="Medium">Medium</option>
-                                <option value="Hard">Hard</option>
-                        </select> <br />
-                        
-                </form>    
-                <button className="submit" onClick={handleSubmit} type="submit">Submit</button>
-
-                </div>
-        
-        </>,
-        document.getElementById('portal')
-
-    )
+  return ReactDom.createPortal(
+    <>
+      <div className="overlay"></div>
+      <div className="modal">
+        <form action="">
+          <label htmlFor="length">Length:</label>
+          <select name="length" value={state.length} onChange={handleChange}>
+            {lengthList.map((d, index) => (
+              <option value={d}>{d}</option>
+            ))}
+          </select>{" "}
+          <br />
+          <label htmlFor="width">Width: </label>
+          <select name="width" value={state.width} onChange={handleChange}>
+            {widthList.map((d) => (
+              <option value={d}>{d}</option>
+            ))}
+          </select>{" "}
+          <br />
+          <label htmlFor="speed">Speed: </label>
+          <select
+            name="length"
+            value={state.difficulty}
+            onChange={handleDifficulty}
+          >
+            <option value="Easy">Easy</option>
+            <option value="Medium">Medium</option>
+            <option value="Hard">Hard</option>
+          </select>{" "}
+          <br />
+        </form>
+        <button className="submit" onClick={handleSubmit} type="submit">
+          Submit
+        </button>
+      </div>
+    </>,
+    document.getElementById("portal")
+  );
 }
-
